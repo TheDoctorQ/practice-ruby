189c620f0836076be4f8ce91e830d4fb30239445
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-01 12:53:00 -0700

    add printed descriptions of items

---
+++ store-item.rb
@@ -0,0 +1,7 @@
+sword = {metal: "vikendi", weight: 90, cost: 100}
+mace = {metal: "mithril", weight: 15, cost: 1000}
+shield = {metal: "steel", weight: 70, cost: 50}
+
+p "Check out this sword, it's made of #{sword[:metal]} metal!"
+p "Now this mace is made of a special metal called #{mace[:metal]}, as a result it's a bit more epensive at #{mace[:cost]} gold, but it only weighs #{mace[:weight]} lbs!"
+p "This is just a standard #{shield[:metal]} shield."
